714f7136b1251712ec71b3ea7faf47c976af6bb0
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-02 23:37:14 +0300

    hello 5

--- Cpp_005/Cpp_005/Program.cs
+++ Cpp_005/Cpp_005/Program.cs
@@ -14,6 +14,8 @@ namespace Cpp_005
             Console.WriteLine("Hello World!");
             // 4
             Console.WriteLine("copy_parents_branch");
+            // 5
+            Console.WriteLine("Hello World 5");
         }
     }
 }
