6ac32d111040c7540ab1dc154ef9c807598d803e
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-04-02 23:21:03 +0300

    Pull ir pirmas VC projetas

---
+++ Cpp_005/Cpp_005/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cpp_005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
